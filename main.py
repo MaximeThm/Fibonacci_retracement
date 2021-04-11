2d655c30f39658c97dea99fef1ffe5636b7dbeef
Maxime <35689674+MaximeThm@users.noreply.github.com>
2021-04-11 20:21:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 import datetime
 
-def get_stock_data(stock, dataShown):
+
+def get_stock_data(stock, datashown):
     yf.pdr_override()
 
     end_date = datetime.date.today()
-    start_date = end_date - datetime.timedelta(days=dataShown)
+    start_date = end_date - datetime.timedelta(days=datashown)
 
     df = pdr.get_data_yahoo(stock, start=start_date, end=end_date)
 
@@ -15,29 +16,30 @@ def get_stock_data(stock, dataShown):
 
     return df
 
+
 # (Max price - lowest price) * golden ratio + lowest price
-def fibonacci(yCoords):
+def fibonacci(y_coords):
     # Collect the amount of X positons there are
     theXs = []
-    for j in range(0, len(yCoords)):
+    for j in range(0, len(y_coords)):
         theXs.append(j)
     # Find the highest price and lowest price
-    maxPrice = max(yCoords)
-    minPrice = min(yCoords)
+    maxPrice = max(y_coords)
+    minPrice = min(y_coords)
     goldenNums = [0.382, 0.50, 0.618, 0.236]
     goldenNums.sort(reverse=True)
-    maxPriceList = [maxPrice] * len(yCoords)
-    minPriceList = [minPrice] * len(yCoords)
+    maxPriceList = [maxPrice] * len(y_coords)
+    minPriceList = [minPrice] * len(y_coords)
     # Plot the 100% fibonacci ceiling
     plt.plot(theXs, maxPriceList, "--", label=('100% - $' + str(round(maxPrice, 2))))
     for i in range(0, len(goldenNums)):
         evalu = (maxPrice - minPrice) * goldenNums[i] + minPrice
-        theYs = [evalu] * len(yCoords)
+        theYs = [evalu] * len(y_coords)
         plt.plot(theXs, theYs, "--", label=(str(goldenNums[i]) + '% - $' + str(round(evalu, 2))))
     # Plot the 0% fibonacci ceiling
     plt.plot(theXs, minPriceList, "--", label='0% - $' + str(round(minPrice, 2)))
 
-# Main
+
 def main():
     # download data from yahoo finance
     print("Welcome! Please either enter the stock ticker (from yahoo finance)")
@@ -66,7 +68,7 @@ def main():
     for i in range(number_of_rows - dataShown, number_of_rows):
         try:
             y = df['Adj Close'][i]
-        except:
+        except Exception:
             y = (df['Adj Close'][i + 1] + df['Adj Close'][i - 1]) / 2
         xCoords.append(x)
         yCoords.append(y)
